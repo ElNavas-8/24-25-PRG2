39e815fe17c680408fb9ffc3b789c2e681bed4ed
Nicol�s Navas Suasnavas <nicolas.navas@alumnos.uneatlantico.es>
2025-02-25 13:48:23 +0100

    Object Set

---
+++ entregas/Calc.java
@@ -0,0 +1,46 @@
+package entregas;
+import java.util.Scanner;
+
+class Calc {
+    public static void main(String[] args) {
+        
+    }
+    
+    public Calculator(){};
+
+    public void connect(){};
+
+    public void userInput(){
+        Scanner scanner = new Scanner(System.in);
+        double number = scanner.nextDouble();
+        return number;
+    }
+
+    public void cleanMemory(){};
+    public void cleanLast(){};
+
+    public void display(){};
+
+    public void sum(){};
+    public void minus(){};
+    public void divide(){};
+    public void multiply(){};
+
+    public void changeOperator(){};
+
+    public void power(){};
+    public void toThePowerOf(){};
+    public void squareRoot(){};
+    public void logarithm(){};
+
+    public void sen(){};
+    public void cos(){};
+    public void tan(){};
+    public void cotan(){};
+    public void sec(){};
+    public void cosec(){};
+
+    public void alternateAngleMode(){};
+    public void alternateNumberFormat(){};
+
+};
