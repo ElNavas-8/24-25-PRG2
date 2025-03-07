cfb3db19bf1c035bf4499b31d0df501f3ae17731
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2025-03-07 10:25:36 +0100

    Charla del Martes 4/03/25

--- entregas/App.java
+++ entregas/App.java
@@ -1,12 +1,40 @@
 package entregas;
 
-class App {
-    public static void main(String[] args) {
-        
-        Calc calculadora = new Calc();
+public class App {
 
-        calculadora.inputNumber(1);
-        System.err.println(calculadora.show());
-        
-    }    
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora(100);
+
+        calculadora.ingresarNumero(5);
+        calculadora.ingresarNumero(3);
+        calculadora.sumar();       
+        System.out.println(calculadora.mostrarTodo());
+
+        calculadora.ingresarNumero(5);
+        calculadora.ingresarNumero(3);
+        calculadora.restar();       
+        System.out.println(calculadora.mostrarTodo());
+
+        calculadora.ingresarNumero(5);
+        calculadora.ingresarNumero(3);
+        calculadora.multiplicar();       
+        System.out.println(calculadora.mostrarTodo());
+
+        calculadora.ingresarNumero(45);
+        calculadora.ingresarNumero(5);
+        calculadora.dividir();       
+        System.out.println(calculadora.mostrarTodo());
+
+        calculadora.calcularSumatoria();
+        System.out.println(calculadora.mostrarTodo());
+
+        for(int i=0;i<15;i++){
+            calculadora.ingresarNumero(Math.random()*100);
+        }
+        System.out.println(calculadora.mostrarTodo());
+
+        calculadora.calcularMedia();
+        System.out.println(calculadora.mostrarTodo());
+    }
 }
